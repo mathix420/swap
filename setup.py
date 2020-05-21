864b70a68fc37ea64b82530b1d3c5b1f2866ffd1
Arnaud Gissinger <agissing@student.42.fr>
2020-05-21 10:20:37 +0200

    [FIX #1] Missing files in sdist

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='swp',
-    version='0.1.2',
+    version='0.1.3',
     description='Simple components sharing tool',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
