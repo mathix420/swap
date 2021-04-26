2f84000ab25c3473d0c292fec2041b87a834156c
Arnaud Gissinger <agissing@student.42.fr>
2021-04-26 11:54:24 +0200

    [UPDATE] remove also from lockfile

--- swap/handlers/remove.py
+++ swap/handlers/remove.py
@@ -1,3 +1,4 @@
+from swap.lock import load_lockfile, save_lockfile
 from swap.config import save_config
 from swap.git import git_add_commit
 
@@ -12,6 +13,8 @@ def remove_component(options: Namespace):
         exit(f'Cannot find the component {name}')
     else:
         results[0][1].pop(name)
+        load_lockfile(options).pop(name)
+        save_lockfile(options)
 
     save_config(options.template, options.c)
 
